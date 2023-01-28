e966b2d5a14f3bd43b2f4222812d57013045cbb9
Elena <lena-k861@yandex.ru>
2023-01-28 15:57:11 +0300

    add If_Else

---
+++ Example005_Condition_If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "лена")
+{
+  Console.WriteLine("Ура, это же Лена!");
+}
+else
+{
+  Console.Write("Привет, ");
+  Console.WriteLine(username);
+}
