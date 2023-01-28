a49fe44852b8b3f3b3f19a72209094def6a3b6c7
Elena <lena-k861@yandex.ru>
2023-01-28 09:48:03 +0300

    add info about Random numbers

--- Examle004_Div/Program.cs
+++ Examle004_Div/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double numberA = new Random().Next(1,10) //1 2 3 4 5....9
+Console.WrineLine(namberA)
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
