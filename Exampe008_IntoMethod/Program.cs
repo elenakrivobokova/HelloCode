ef5d3ad930d665cdc62609eb619d5ed8e8eca576
Elena <lena-k861@yandex.ru>
2023-02-01 00:11:41 +0300

    add Example009

---
+++ Exampe008_IntoMethod/Program.cs
@@ -0,0 +1,29 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+  int result = arg1;
+  if (arg2 > result) result = arg2;
+  if (arg3 > result) result = arg3;
+  return result;
+}
+
+int a1 = 15;
+int b1 = 21;
+int c1 = 2;
+int a2 = 10;
+int b2 = 31;
+int c2 = 25;
+int a3 = 75;
+int b3 = 211;
+int c3 = 202;
+
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
+
+int max = Max(
+  Max(a1, b1, c1),
+  Max(a2, b2, c2),
+  Max(a3, b3, c3));
+
+System.Console.WriteLine(max);
